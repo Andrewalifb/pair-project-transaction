6c8192ed9fa72bd1fcb96d3c2776786ea1b391c1
yohanesrickyyy <yohanesrickyyy@gmail.com>
2024-04-26 10:37:27 +0700

    feat(handlers):Transaction, UPdated

--- handler/handler.go
+++ handler/handler.go
@@ -9,8 +9,10 @@ import (
 	"github.com/Andrewalifb/pair-project-transaction/model"
 	"github.com/labstack/echo/v4"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+// ANDREW
 func CreateTransaction(c echo.Context) error {
 	collectionName := os.Getenv("TRANSACTION_COLLECTION")
 	collection, err := config.ConnectionDatabase(context.Background(), collectionName)
@@ -41,19 +43,16 @@ func GetAllTransaction(c echo.Context) error {
 
 	var datas []model.Transaction
 
-
 	cursor, err := collection.Find(context.Background(), bson.D{{}})
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
-
 
 	for cursor.Next(context.Background()) {
 		var data model.Transaction
 		if err := cursor.Decode(&data); err != nil {
 			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 		}
-
 
 		datas = append(datas, data)
 	}
@@ -63,4 +62,58 @@ func GetAllTransaction(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusCreated, datas)
+}
+
+// Yohanes
+// GET transcation by ID
+func GetTransactionByID(c echo.Context) error {
+	collectionName := os.Getenv("TRANSACTION_COLLECTION")
+	collection, err := config.ConnectionDatabase(context.Background(), collectionName)
+	if err != nil {
+		return err
+	}
+
+	id, err := primitive.ObjectIDFromHex(c.Param("id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	var data model.Transaction
+
+	err = collection.FindOne(context.Background(), bson.M{"_id": id}).Decode(&data)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusOK, data)
+}
+
+// UPDATE
+func UpdateDataTransaction(c echo.Context) error {
+	collectionName := os.Getenv("TRANSACTION_COLLECTION")
+	collection, err := config.ConnectionDatabase(context.Background(), collectionName)
+	if err != nil {
+		return err
+	}
+
+	id, err := primitive.ObjectIDFromHex(c.Param("id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	p := new(model.Transaction)
+	err = c.Bind(p)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	// Proses Update Data
+	result, err := collection.UpdateOne(
+		context.Background(),
+		bson.M{"_id": id},
+		bson.M{"$set": p},
+	)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusCreated, result)
 }
